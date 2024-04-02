4208907d0efbb2995b2ed9eafd158960739e1c11
Cameron Chase Naidoo <125469506+ChaseNaidoo@users.noreply.github.com>
2024-04-02 15:44:01 +0200

    Update webpack.config.js

--- 0x00-Webpack/task_3/webpack.config.js
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -4,27 +4,21 @@ module.exports = {
   mode: 'production',
   entry: './js/dashboard_main.js',
   output: {
-    path:path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js'
-  },
-  performance: {
-    maxAssetSize: 1000000,
   },
   module: {
     rules: [
       {
-        test: /\.css$/i,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
       },
       {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        use: [
-          {loader: 'image-webpack-loader'} //["file-loader"]
-        ]
+        test: /\.jpg$/,
+        use: {
+          loader: 'file-loader'
+        }
       }
     ]
   }
-}
+};
