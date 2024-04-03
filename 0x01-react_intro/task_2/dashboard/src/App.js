a6b5ce85bf8c04dd38fd06fbcdf0fb3613313774
ChaseNaidoo <chasenaidoo9@gmail.com>
2024-04-03 16:47:14 +0200

    Modified utils.js and Notifications.js

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -7,12 +7,17 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App.H1"> School dashboard</h1>
+        <h1 className="App.H1">School dashboard</h1>
       </header>
       <body className="App-body">
         <p>
           Login to access the full dashboard
         </p>
+        <label className="email" htmlFor='email'>Email: </label>
+        <input type="email" id="email" name="email"/>
+        <label className="password" htmlFor='password'>Password: </label>
+        <input type="password" id="password" name="password"/>
+        <button className="label-button">OK</button>
       </body>
       <footer className="App-footer">
         <p>
