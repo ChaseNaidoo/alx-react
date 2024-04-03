d9343f32e9156d0bf55ff3d45a545fbe51064f8e
ChaseNaidoo <chasenaidoo9@gmail.com>
2024-04-03 16:16:43 +0200

    Modify App.js

--- 0x01-react_intro/task_2/dashboard/src/App.js
+++ 0x01-react_intro/task_2/dashboard/src/App.js
@@ -1,5 +1,4 @@
 import logo from './Holberton_Logo.jpg';
-import {getFullYear, getFooterCopy} from './utils.js';
 import './App.css';
 
 function App() {
@@ -7,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1 className="App.H1"> School dashboard</h1>
+        <h1 className="App.H1">School dashboard</h1>
       </header>
       <body className="App-body">
         <p>
@@ -16,7 +15,7 @@ function App() {
       </body>
       <footer className="App-footer">
         <p>
-          Copyright {getFullYear()} - {getFooterCopy(true)}
+          Copyright 2020 - holberton School
         </p>
       </footer>
     </div>
