6bcff95b03730e361550602bf8ecb14cdef2871d
Cameron Chase Naidoo <125469506+ChaseNaidoo@users.noreply.github.com>
2024-04-19 13:56:34 +0200

    Update Footer.js

--- 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
+++ 0x05-react_state/task_3/dashboard/src/Footer/Footer.js
@@ -1,15 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./Footer.css";
 import { getFullYear, getFooterCopy } from "../utils/utils";
+import { AppContext } from "../App/AppContext";
 
 function Footer() {
-    return (
-	    <>
-	    <div className="App-footer">
-	    Copyright {getFullYear()} - {getFooterCopy()}
-	</div>
-	    </>
-    );
+  const { user } = useContext(AppContext);
+
+  return (
+    <>
+      <div className="App-footer">
+        {user.isLoggedIn && (
+          <p>
+            <a href="#">Contact us</a>
+          </p>
+        )}
+        <p>
+          Copyright {getFullYear()} - {getFooterCopy()}
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default Footer;
