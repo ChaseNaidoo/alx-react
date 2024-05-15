4db3a86d74d588b13534f61d433f6463153c8c2b
Cameron Chase Naidoo <125469506+ChaseNaidoo@users.noreply.github.com>
2024-05-15 13:31:24 +0200

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,4 +1,21 @@
 import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App/App";
+import uiReducer, { initialState } from "./reducers/uiReducer";
+import { Map } from "immutable";
+
+const store = createStore(uiReducer, Map(initialState));
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App/App";
 
