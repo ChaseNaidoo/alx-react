b4a9143e9eb64deddc08f9c160fde9e498bf34a8
Cameron Chase Naidoo <125469506+ChaseNaidoo@users.noreply.github.com>
2024-05-15 13:32:03 +0200

    Update index.js

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -15,13 +15,4 @@ ReactDOM.render(
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
-);import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App/App";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-	<React.StrictMode>
-	<App />
-	</React.StrictMode>
 );
